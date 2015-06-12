fee4657dafba3b7398bc41cab1be31cf502ad7e9
Anatolii Stepaniuk <anatolii.stepaniuk@gmail.com>
2015-06-12 13:59:01 +0300

    Merging fixes

--- src/com/tutorial/main/Player.java
+++ src/com/tutorial/main/Player.java
@@ -3,30 +3,30 @@ package com.tutorial.main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-
 public class Player extends GameObject{
 	private int hurtCounter = 0;
-
+	private int healCounter = 0;
 	public Player(int x, int y, ID id, Handler myHandler) {
 		super(x, y, id, myHandler);
 	}
-	
+
 	public void tick(){
-			x += velX;
-			y += velY;	
-		
+		x += velX;
+		y += velY;
+
 		x = Game.clamp(x, 0, Game.WIDTH-32);
 		y = Game.clamp(y, 0, Game.HEIGHT-60);
 		collision();
-
 		if(hurtCounter > 0)
 			hurtCounter--;
+		if(healCounter > 0)
+			healCounter--;
 	}
-	private void collision(){
-		for(int i = 0; i < Game.gameHandler.objectList.size(); i++){
+	private void collision() {
+		for (int i = 0; i < Game.gameHandler.objectList.size(); i++) {
 			GameObject tempObject = Game.gameHandler.objectList.get(i);
-			if(tempObject.id == ID.BasicEnemy || tempObject.id == ID.FastEnemy || tempObject.id == ID.SmartEnemy || tempObject.id == ID.BossEnemy){
-				if(getBounds().intersects(tempObject.getBounds())){
+			if (tempObject.id == ID.BasicEnemy || tempObject.id == ID.FastEnemy || tempObject.id == ID.SmartEnemy) {
+				if (getBounds().intersects(tempObject.getBounds())) {
 					// collision code
 					Game.hud.HEALTH -= 2;
 					Game.sound.sayOuche();
@@ -38,25 +38,48 @@ public class Player extends GameObject{
 				if (getBounds().intersects(tempObject.getBounds())) {
 					Game.hud.HEALTH += ((HealthCoin) tempObject).getHealthValue();
 					Game.gameHandler.removeObject(tempObject);
+					Game.sound.playHealthSound();
+					healCounter = 30;
+					if (Game.hud.HEALTH == 0)
+						healCounter = 0;
+				}
+			} else if (tempObject.id == ID.ScoreCoin) {
+				if (getBounds().intersects(tempObject.getBounds())) {
+					Game.hud.setScore(Game.hud.getScore() + ((ScoreCoin) tempObject).getScoreValue());
+					Game.gameHandler.removeObject(tempObject);
+					Game.sound.playHealthSound();
+					healCounter = 30;
+					if (Game.hud.HEALTH == 0)
+						healCounter = 0;
 				}
 			}
 		}
 	}
-	
+
 	public void render(Graphics g){
-
-		 // Makes player blink red when hurted
-	if(hurtCounter/5 == 5 || hurtCounter/5 == 3 || hurtCounter/5 == 1){
-		g.setColor(Color.red);
-		g.fillRect(x, y, 32, 32);
+		if(hurtCounter > 0)
+			blinkRed(g, hurtCounter);
+		else if(healCounter > 0)
+			blinkGreen(g, healCounter);
+		else
+		{
+			g.setColor(Color.white);
+			g.fillRect(x, y, 32, 32);
+		}
 	}
-	else{
-		g.setColor(Color.white);
-		g.fillRect(x, y, 32, 32);
-	}
-}
-
 	public Rectangle getBounds() {
 		return new Rectangle(x,y,32,32);
+	}
+	private void blinkRed(Graphics g, int hurtCounter){
+		if(hurtCounter/5 == 5 || hurtCounter/5 == 3 || hurtCounter/5 == 1){
+			g.setColor(Color.red);
+			g.fillRect(x, y, 32, 32);
+		}
+	}
+	private void blinkGreen(Graphics g, int healCounter){
+		if(healCounter/5 == 5 || healCounter/5 == 3 || healCounter/5 == 1){
+			g.setColor(Color.green);
+			g.fillRect(x, y, 32, 32);
+		}
 	}
 }
